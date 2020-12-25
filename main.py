f0d6499744bca43d24dde69ff05e82430048418c
Gagan Malvi <malvigagan@gmail.com>
2020-12-25 09:48:59 +0530

    cartel_installer: Misc fixes and improvements
    * Add safeguards to ROM and Recovery Installation Methods
    * Tested it with the Xiaomi Redmi Note 4 (mido)
    * Added file download function
    * Moved from OS functions to subprocess.run
    * Full rewrite
    
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- main.py
+++ main.py
@@ -1,67 +1,90 @@
-'''
-Copyright (C) 2020 Gagan Malvi
-Licensed under the Cartel Project Public License available at
-https://github.com/CartelProject/CPL
-All rights reserved
-I am hereby NOT responsible for any damage caused whatsoever by this tool
-and you're solely responsible because you decided to use it in the first 
-place. So if you scream at me, I will laugh at you.
-'''
+import urllib.request as r
+import subprocess
 
-import os
+def downloadFile(link, path):
+    print('Beginning download of ROM...')
+    r.urlretrieve(link, path)
+
+def getDeviceCodename():
+    result = subprocess.run(['adb', 'shell', 'getprop', 'ro.build.product'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    print('The connected device is: ', result)
 
 def ROMInstall():
-    print('Running ROM installation, do not unplug your device or power it off.')
-    os.system('adb reboot sideload')
-    os.system('adb sideload rom.zip')
-    print('ROM successfully sideloaded, if the device does not reboot, manually reboot to system.')
-    Intro()
+    getDeviceCodename()
+    print('THIS ACTION WILL POTENTIALLY BREAK AND DELETE PARTITIONS AND ITS CONTENTS. DO YOU WANT TO CONTINUE?')
+    x = input('Are you sure to continue? (Y/N)')
+    if x.capitalize() == 'Y':
+        print('Running ROM installation, do not unplug your device or power it off.')
+        result = subprocess.run(['adb', 'reboot', 'sideload'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        print(result)
+        print('STORE THE ROM ZIP IN THE DIRECTORY OF THE SCRIPT, OTHERWISE THE SCRIPT WILL FAIL.')
+        input('Once you see device rebooting to recovery, press enter.')
+        name = input('Enter ROM zip filename: ') # Should be saved as rom.zip if it is downloaded by the downloadFile function
+        result = subprocess.run(['adb', 'sideload', name], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        print(result)
+        print('ROM successfully sideloaded, if the device does not reboot, manually reboot to system.')
+        Intro()
+    else:
+        Intro()
+
 
 def RcvryInstall():
-    print('Running Recovery installation, do not unplug your device or power it off')
-    os.system('adb reboot-bootloader')
-    os.system('fastboot flash recovery recovery.img')
-    userInput = input('Do you want to boot to recovery or proceed to reboot to system? [Y/N]')
-    if userInput.capitalize() == "Y":
-        print('Booting to recovery...')
-        os.system('fastboot boot recovery.img')
-        print('Successfully booted to recovery.')
-    elif userInput.capitalize() == "N":
-        print('Booting to system...')
-        os.system('fastboot reboot')
-        print('Successfully booted to system.')
+    print('THIS ACTION WILL POTENTIALLY BREAK AND DELETE PARTITIONS AND ITS CONTENTS. DO YOU WANT TO CONTINUE?')
+    x = input('Are you sure to continue? (Y/N)')
+    if x.capitalize() == 'Y':    
+        print('Running Recovery installation, do not unplug your device or power it off')
+        result = subprocess.run(['adb', 'reboot', 'bootloader'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        input('Once device has reached bootloader state, press enter to continue')
+        result = subprocess.run(['fastboot', 'flash', 'recovery', 'recovery.img'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+        userInput = input('Do you want to boot to recovery or proceed to reboot to system? [Y/N]')
+        if userInput.capitalize() == "Y":
+            print('Booting to recovery...')
+            result = subprocess.run(['fastboot', 'boot', 'recovery.img'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+            print(result)
+            print('Successfully booted to recovery.')
+        elif userInput.capitalize() == "N":
+            print('Booting to system...')
+            result = subprocess.run(['fastboot', 'reboot'], stdout=subprocess.PIPE).stdout.decode('utf-8')
+            print(result)
+            print('Successfully booted to system.')
+        else:
+            print('Wrong choice.')
+        Intro()
     else:
-        print('Wrong choice.')
-    Intro()
+        Intro()
 
 def Intro():
     print('====================')
     print('Cartel ROM Installer')
     print('====================\n')
     print('A simple ROM installer for Android devices that are supported by the Cartel Project.\n')
+    getDeviceCodename()
     print('Select your choice:\n')
     print(' [1] Install Cartel ROM.')
     print(' [2] Install a compatible recovery.')
-    # TODO: DOWNLOAD FILES AUTOMATICALLY
-    print(' [3] Exit')
+    print(' [3] Download Cartel ROM from a mirror of your choice.')
+    # TODO: Download files automatically by checking the codename of the device using getDeviceCodename()
+    print(' [4] Exit')
     userInput = int(input('Enter your selection [1/2/3]:'))
     if userInput == 1:
-        print('Store ROM zip as rom.zip in the root directory of the script.')
-        input('Press any key to continue...')
         ROMInstall()
     elif userInput == 2:
         print('Store recovery IMG as recovery.img in root directory of the script.')
         input('Press any key to continue...')
         RcvryInstall()
     elif userInput == 3:
+        url = input('Enter URL of file: ')
+        path = 'rom.zip'
+        downloadFile(url,path)
+    elif userInput == 4:
         x = input('Are you sure to exit? (Y/N)')
         if x.capitalize() == 'Y':
             exit()
         else:
             Intro()
+    else:
+        Intro()
 
 Intro()
-
-
 
     
